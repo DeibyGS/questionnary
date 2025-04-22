4482a46d1a96b2401511bdc6adad1f9913966841
Deibyg <40776476+deiybg@users.noreply.github.com>
2025-04-22 18:37:19 +0200

    Eliminados index.html y main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1, 2, 3, 4, 5];
-const i = 3,
-  j = 1;
-
-console.log("Antes del intercambio:", numbers);
-
-[numbers[i], numbers[j]] = [numbers[j], numbers[i]];
-
-console.log("Después del intercambio:", numbers);
